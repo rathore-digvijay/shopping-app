361eff14e28b9dac4f95d95fdc12f79425759f63
Digvijay <rathore.digvijay10@gmail.com>
2020-05-05 21:44:03 +0530

    small fix

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,3 @@
-/*
- * File: index.js
- * Project: ecommerce-app
- * File Created: Monday, 4th May 2020 8:47:19 pm
- * Author: Digvijay Rathore (rathore.digvijay10@gmail.com)
- */
-
 var express = require('express');
 var router = express.Router();
 const dbQuery = require('../../database/dbQuery.js')
